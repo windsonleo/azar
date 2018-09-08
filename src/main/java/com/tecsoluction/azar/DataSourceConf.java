53d441d3b20c2efe7943280c7fb8bcc7e2506168
windsonleo <windsor_melo@hotmail.com>
2018-09-08 20:09:50 -0300

    DataSource
    DataSource

--- src/main/java/com/tecsoluction/azar/DataSourceConf.java
+++ src/main/java/com/tecsoluction/azar/DataSourceConf.java
@@ -27,23 +27,23 @@ public class DataSourceConf {
 	
     public DataSource dataSource() {
    
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("org.postgresql.Driver");
-//        dataSource.setUrl("jdbc:postgresql://ec2-54-227-244-12.compute-1.amazonaws.com:5432/d7gaoujdb4o1lp");
-//        dataSource.setUsername("zhrzxiwajhepyq");
-//        dataSource.setPassword("89f7d7617d5f3955bdbf7565411e7c156573b68ad9fe3df179ba18b95c7f269f");
-        
-        
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/azzar?useSSL=true");
-//        dataSource.setUrl("jdbc:mysql://node148635-jogodeazar.jelasticlw.com.br/azzar");
-
-        dataSource.setUsername("root");
-//        dataSource.setPassword("ALEbly31071");
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://ec2-54-227-244-12.compute-1.amazonaws.com:5432/d7gaoujdb4o1lp");
+        dataSource.setUsername("zhrzxiwajhepyq");
+        dataSource.setPassword("89f7d7617d5f3955bdbf7565411e7c156573b68ad9fe3df179ba18b95c7f269f");
         
+        
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/azzar?useSSL=true");
+////        dataSource.setUrl("jdbc:mysql://node148635-jogodeazar.jelasticlw.com.br/azzar");
+//
 //        dataSource.setUsername("root");
-        dataSource.setPassword("123456");
+////        dataSource.setPassword("ALEbly31071");
+//        
+////        dataSource.setUsername("root");
+//        dataSource.setPassword("123456");
 
     	return dataSource;
     }
@@ -70,11 +70,11 @@ public class DataSourceConf {
 
         va.setGenerateDdl(true);
         va.setShowSql(true);
-        va.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
-//        va.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+//        va.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
+        va.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
         Properties ps = new Properties();
-        ps.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
-//        ps.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+//        ps.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        ps.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         ps.put("spring.jpa.hibernate.ddl-auto", "update");
 		ps.put("useSSL","false");
 //		ps.put("security.basic.enabled","false");
